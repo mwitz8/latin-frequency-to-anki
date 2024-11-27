f1de2545360ca9ccd2c14244a3c19a4c4fd6e22c
mwitz8 <mattwitz888@gmail.com>
2024-11-27 12:05:34 -0800

    Analyzes and serializes corpus

--- main.py
+++ main.py
@@ -1,3 +1,31 @@
+import os
+from pathlib import Path
+import pickle
+from progress.bar import Bar
+from progress.spinner import PieSpinner
+from cltk import NLP
 from cltk.data.fetch import FetchCorpus
+
 corpus_downloader = FetchCorpus(language="lat")
-corpus_downloader.import_corpus('lat_text_perseus')
+corpus_downloader.import_corpus('lat_text_latin_library')
+cltk_nlp = NLP(language="lat", suppress_banner=True)
+
+file_count = 0
+for file_path in Path(os.path.join(os.path.expanduser('~'), 'cltk_data', 'lat', 'text', 'lat_text_latin_library')).rglob('*.txt'):
+    file_count = file_count + 1
+data = []
+with Bar('analyzing texts...', max=file_count) as progress:
+    print('\n')
+    for file_path in Path(os.path.join(os.path.expanduser('~'), 'cltk_data', 'lat', 'text', 'lat_text_latin_library')).rglob('*.txt'):
+        with open(file_path, 'r') as file:
+            cltk_doc = cltk_nlp.analyze(text=file.read())
+            print('analyzed')
+            data.append(cltk_doc)
+        progress.next()
+
+with PieSpinner('serializing data...') as progress:
+    open('data', 'w').close()
+    data_file = open(Path('data'), 'ab')
+    pickle.dump(data, data_file)
+    data_file.close()
+    progress.next()
